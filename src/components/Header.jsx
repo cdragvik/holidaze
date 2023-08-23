093556fdb5dd850a9fc0358ad8478765ae12e5ff
cdragvik <cndragvik@gmail.com>
2023-08-23 13:43:20 +0200

    logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,78 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Logo from "../assets/holidaze-logo.png"; 
-import { load } from "../api/storage";
+import Logo from "../assets/holidaze-logo.png";
+import { clear, load } from "../api/storage";
+
 
 const NavBar = styled.nav`
-    background-color: #213555;
+  background-color: #213555;
 `;
 
 const LogoImg = styled.img`
-    height: 120px;
-    padding: 20px;
-    display: flex;
-    margin: auto;
+  height: 120px;
+  padding: 20px;
+  display: flex;
+  margin: auto;
 `;
 
 const Navigation = styled.nav`
-    ul {
-        list-style: none;
-        display: flex;
-        margin: 0;
-        padding: 0;
-    }
+  ul {
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+  }
 
-    li {
-        margin-right: 20px;
-    }
+  li {
+    margin-right: 20px;
+  }
 
-    a {
-        color: white;
-        text-decoration: none;
-        font-weight: bold;
-    }
+  a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+  }
 `;
 
 export default function Header() {
-    const user = load('profile'); // Load the user's profile from local storage or your authentication context
+  const user = load("profile"); // Load the user's profile from local storage or your authentication context
 
-    return ( 
-        <NavBar>
-            <a href="/"><LogoImg src={Logo} alt="Holidaze Logo" /></a>
-            <Navigation>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    {user ? (
-                        <>
-                            <li><Link to={`/profile/${user.name}`}>Profile</Link></li> 
-                            <li><a href="/logout">Logout</a></li>
-                        </>
-                    ) : (
-                        <li><a href="/login">Log in</a></li> 
-                    )}
-                </ul>
-            </Navigation>
-        </NavBar>
-    );
+  const handleLogout = () => {
+    clear(); // Clear user's token and profile from local storage
+
+    // Redirect to the login page
+    window.location.href = "/login";
+  };
+
+  return (
+    <NavBar>
+      <a href="/">
+        <LogoImg src={Logo} alt="Holidaze Logo" />
+      </a>
+      <Navigation>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <Link to={`/profile/${user.name}`}>Profile</Link>
+              </li>
+              <li>
+                <a href="#!" onClick={handleLogout}>
+                  Logout
+                </a>
+              </li>
+            </>
+          ) : (
+            <li>
+              <a href="/login">Log in</a>
+            </li>
+          )}
+        </ul>
+      </Navigation>
+    </NavBar>
+  );
 }
