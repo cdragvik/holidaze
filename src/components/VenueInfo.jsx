591726d9bfb7d3133b0c7341fdadcf04362dda68
Cdragvik <cndragvik@gmail.com>
2023-08-29 23:05:12 +0200

    Moved venue info

---
+++ src/components/VenueInfo.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { VenueInfoContainer, Location, Title, ImageContainer, InfoRow, FeatureList, Image, Feature } from "../styles/Venue";
+
+const VenueInfo = ({ venue }) => {
+  return (
+
+    <VenueInfoContainer>    
+        
+        <Title>{venue?.name}</Title>
+        <Location>{venue?.location?.city}, {venue?.location?.country}</Location>
+        
+        <ImageContainer>
+            <Image src={venue?.media?.[0]} alt={venue?.name} />
+        </ImageContainer>
+
+        <p>{venue?.description}</p>
+
+        <InfoRow>
+            <span>Price:</span> <span>${venue?.price} /night</span>
+        </InfoRow>
+        <InfoRow>
+            <span>Max Guests:</span> <span>{venue?.maxGuests}</span>
+        </InfoRow>
+        <InfoRow>
+            <span>Rating:</span> <span>{venue?.rating} ★</span>
+        </InfoRow>
+
+        <FeatureList>
+            {venue?.meta?.wifi && <Feature>WiFi</Feature>}
+            {venue?.meta?.parking && <Feature>Parking</Feature>}
+            {venue?.meta?.breakfast && <Feature>Breakfast</Feature>}
+            {venue?.meta?.pets && <Feature>Pets Allowed</Feature>}
+        </FeatureList>
+
+        {venue?.owner && (
+            <div>
+                <h3>Owner Information</h3>
+                <p>Name: {venue.owner.name}</p>
+                <p>Email: {venue.owner.email}</p>
+            </div>
+        )}
+
+    </VenueInfoContainer>
+  );
+};
+
+export default VenueInfo;
--- src/pages/VenuePage.jsx
+++ src/pages/VenuePage.jsx
@@ -6,6 +6,7 @@ import { Card, FormGroup, Input, Label, SubmitButton, TextArea } from "../styles
 import { CustomCalendar, Feature, FeatureList, GuestsInput, GuestsLabel, Image, ImageContainer, InfoRow, Location, PageContainer, StyledTable, Title, VenueInfoContainer } from "../styles/Venue";
 
 import "react-datepicker/dist/react-datepicker.css";
+import VenueInfo from "../components/VenueInfo";
 
 
 
@@ -154,40 +155,10 @@ const VenuePage = () => {
   return (
     <Layout>
       <PageContainer>
-        <Title>{venue?.name}</Title>
-        <Location>{venue?.location?.city}, {venue?.location?.country}</Location>
-        <VenueInfoContainer>
-          <ImageContainer>
-            <Image src={venue?.media?.[0]} alt={venue?.name} />
-          </ImageContainer>
 
-          <p>{venue?.description}</p>
+      <VenueInfo venue={venue} />
 
-          <InfoRow>
-            <span>Price:</span> <span>${venue?.price} /night</span>
-          </InfoRow>
-          <InfoRow>
-            <span>Max Guests:</span> <span>{venue?.maxGuests}</span>
-          </InfoRow>
-          <InfoRow>
-            <span>Rating:</span> <span>{venue?.rating} ★</span>
-          </InfoRow>
 
-          <FeatureList>
-            {venue?.meta?.wifi && <Feature>WiFi</Feature>}
-            {venue?.meta?.parking && <Feature>Parking</Feature>}
-            {venue?.meta?.breakfast && <Feature>Breakfast</Feature>}
-            {venue?.meta?.pets && <Feature>Pets Allowed</Feature>}
-          </FeatureList>
-
-          {venue?.owner && (
-            <div>
-              <h3>Owner Information</h3>
-              <p>Name: {venue.owner.name}</p>
-              <p>Email: {venue.owner.email}</p>
-            </div>
-          )}
-        
           {isEditing ? (
             <form onSubmit={handleEdit}>
               <Card>
@@ -322,7 +293,7 @@ const VenuePage = () => {
 
 
 
-        </VenueInfoContainer>
+        
       </PageContainer>
     </Layout>
   );
