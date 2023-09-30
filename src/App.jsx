6f920a349b1ca626a7e5ca6d401c453d227db7ec
cdragvik <cndragvik@gmail.com>
2023-10-01 00:09:48 +0200

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import VenuePage from "./pages/VenuePage";
 import LoginForm from "./pages/Login";
